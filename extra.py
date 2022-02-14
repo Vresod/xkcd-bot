5a7f563f2e8daac8ea6b89bbbe77739edd4910d6
vresod <lukebinkofsky@gmail.com>
2022-02-13 19:00:44 -0500

    update to use pycord

--- extra.py
+++ extra.py
@@ -1,6 +1,55 @@
-import aiohttp
+# import aiohttp
 import requests
+import aiohttp
+from discord import Embed, ui, Interaction, Button
+from functools import cache
 
 def get_latest_comic() -> int:
 	r = requests.get("https://xkcd.com/info.0.json").json()
 	return r['num']
+@cache
+def generate_embed(num,title,img_url,alt) -> Embed:
+	embed = Embed(title=f"xkcd {num}: {title}",url=f"https://xkcd.com/{num}")
+	embed.set_image(url=img_url)
+	embed.set_footer(text=alt)
+	embed.description = f"explained: https://explainxkcd.com/{num}"
+	return embed
+
+class ControlsView(ui.View):
+	def __init__(self):
+		super().__init__(timeout=None)
+	
+	@ui.button(label="< Prev")
+	async def _previous(self,button:Button,interaction:Interaction):
+		embed:Embed = interaction.message.embeds[0]
+		new_comic_number = int(embed.title.split(":")[0].replace("xkcd ","")) - 1
+		async with aiohttp.ClientSession() as session: # I personally feel this looks unclean but it does context managers so whatever
+			try:
+				async with session.get(f"https://xkcd.com/{new_comic_number}/info.0.json") as r:
+					r.raise_for_status()
+					comic = await r.json()
+			except aiohttp.ClientResponseError:
+				return
+		embed = generate_embed(comic['num'],comic['title'],comic['img'],comic['alt'])
+		await interaction.message.edit(embeds=[embed])
+	
+	@ui.button(label="Next >")
+	async def _next(self,button:Button,interaction:Interaction):
+		embed:Embed = interaction.message.embeds[0]
+		new_comic_number = int(embed.title.split(":")[0].replace("xkcd ","")) + 1
+		async with aiohttp.ClientSession() as session: # I personally feel this looks unclean but it does context managers so whatever
+			try:
+				async with session.get(f"https://xkcd.com/{new_comic_number}/info.0.json") as r:
+					r.raise_for_status()
+					comic = await r.json()
+			except aiohttp.ClientResponseError:
+				return
+		embed = generate_embed(comic['num'],comic['title'],comic['img'],comic['alt'])
+		await interaction.message.edit(embeds=[embed])
+
+# class ForwardButton(ui.Button):
+# 	async def callback(self, interaction: Interaction):
+# 		embed:Embed = interaction.message.embeds[0]
+# 		comic_number = 
+# 		await interaction.edit_original_message(embeds=[embed])
+# 		return await super().callback(interaction)
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from os import name
 import discord
 from discord.ext import commands, tasks
-from discord_slash import SlashContext, SlashCommand
 import aiohttp
 import logging
-import xml.etree.ElementTree as ET
 
-from extra import get_latest_comic
+import extra
 
+# global latest_comic
+latest_comic = extra.get_latest_comic()
 logging.basicConfig(level=logging.INFO)
-client = commands.Bot(" ")
-slash = SlashCommand(client,sync_commands=True)
+client = commands.Bot()
 latest_comic = 0
 
 @client.event
@@ -18,27 +16,27 @@ async def on_ready():
 	logging.info(f"Signed in as {client.user}")
 	check_for_new_comics.start()
 
-@slash.slash(name="get_comic")
-async def _get_comic(ctx:SlashContext,comic_number:int):
+@client.slash_command(name="get_comic")
+async def _get_comic(ctx:discord.ApplicationContext,comic_number:int):
 	async with aiohttp.ClientSession() as session: # I personally feel this looks unclean but it does context managers so whatever
 		try:
 			async with session.get(f"https://xkcd.com/{comic_number}/info.0.json") as r:
 				r.raise_for_status()
 				comic = await r.json()
 		except aiohttp.ClientResponseError:
-			await ctx.send("Comic not found.",hidden=True)
+			await ctx.respond("Comic not found.",ephemeral=True)
 			return
 	embed = discord.Embed(title=f"xkcd {comic['num']}: {comic['title']}",url=f"https://xkcd.com/{comic_number}")
 	embed.set_image(url=comic['img'])
 	embed.set_footer(text=comic['alt'])
 	embed.description = f"explained: https://explainxkcd.com/{comic_number}"
-	await ctx.send(embeds=[embed])
+	await ctx.respond(embeds=[embed],view=extra.ControlsView())
 
-@slash.slash(name="add_comics_channel")
+@client.slash_command(name="add_comics_channel")
 @commands.has_guild_permissions(manage_channels=True)
-async def _add_comics_channel(ctx:SlashContext,channel:discord.TextChannel):
+async def _add_comics_channel(ctx:discord.ApplicationContext,channel:discord.TextChannel):
 	if channel.type != discord.ChannelType.text:
-		await ctx.send("Choose a channel that is a text channel.",hidden=True)
+		await ctx.respond("Choose a channel that is a text channel.",ephemeral=True)
 		return
 	with open("guilds.txt","r") as guildsfile: # this whole structure sucks and i wish that i knew a better option
 		guilds = list(set(guildsfile.read().split(","))) # not memory efficent solution to remove duplicates
@@ -46,24 +44,25 @@ async def _add_comics_channel(ctx:SlashContext,channel:discord.TextChannel):
 		guilds = [i for i in guilds if i]
 	with open("guilds.txt","w") as guildsfile:
 		guildsfile.write(','.join(guilds))
-	await ctx.send(f"{channel.name} will now receive an update when a comic comes out!")
+	await ctx.respond(f"{channel.name} will now receive an update when a comic comes out!")
 	logging.info(f"Comics channel {channel.id} added")
 
-@slash.slash(name='remove_comics_channel')
+@client.slash_command(name='remove_comics_channel')
 @commands.has_guild_permissions(manage_channels=True)
-async def _remove_comics_channel(ctx:SlashContext,channel:discord.TextChannel):
+async def _remove_comics_channel(ctx:discord.ApplicationContext,channel:discord.TextChannel):
 	if channel.type != discord.ChannelType.text:
-		await ctx.send("Choose a channel that is a text channel.",hidden=True)
+		await ctx.respond("Choose a channel that is a text channel.",ephemeral=True)
 		return
 	with open("guilds.txt") as guildsfile:
 		guilds = list(set(guildsfile.read().split(","))) # mess of parentheses; reader I can assure you i am not trying to write lisp
 		try:
 			guilds.remove(str(channel.id))
 		except ValueError: 
-			await ctx.send("That channel already wasn't receiving updates. :(",hidden=True)
+			await ctx.respond("That channel already wasn't receiving updates. :(",ephemeral=True)
+			return
 	with open("guilds.txt","w") as guildsfile:
 		guildsfile.write(','.join(guilds))
-	await ctx.send(f"{channel.name} will no longer receive an update when a comic comes out. :(")
+	await ctx.respond(f"{channel.name} will no longer receive an update when a comic comes out. :(")
 
 @tasks.loop(minutes=30)
 async def check_for_new_comics():
@@ -85,7 +84,7 @@ async def post_new_comics(comic):
 	embed.set_footer(text=comic['alt'])
 	embed.description = f"explained: https://explainxkcd.com/{comic['num']}"
 	for guild in guilds:
-		channel:discord.abc.GuildChannel = await client.fetch_channel(guild)
+		channel:discord.TextChannel = await client.fetch_channel(guild)
 		await channel.send(embed=embed)
 
 def main():
@@ -93,7 +92,7 @@ def main():
 	with open("guilds.txt","a"): pass # simply ensure the file exists
 	values = dotenv.dotenv_values(".env")
 	global latest_comic
-	latest_comic = get_latest_comic()
+	latest_comic = extra.get_latest_comic()
 	client.run(values['TOKEN'])
 
 if __name__ == "__main__":
